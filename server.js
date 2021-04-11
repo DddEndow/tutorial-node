4f086a1c5f01b5a1cb34e084de13b785c35346ae
遠藤 大輔 [YUMEMI] / Endo Daisuke <d_endo@yumemi.co.jp>
2021-04-11 12:28:40 +0900

    [feat]ファイルをアップロードし、ローカルに保存する処理を追加

--- server.js
+++ server.js
@@ -16,21 +16,9 @@ const url = require('url');
 
 function start(route, handle) {
     function onRequest(request, response) {
-        var postDate = "";
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
-
-        request.setEncoding("utf8");
-
-        request.addListener("data", function (postDataChunk) {
-            postDate += postDataChunk;
-            console.log("Receievd POST data chunk '" +
-            postDataChunk + "'");
-        })
-
-        request.addListener("end", function () {
-            route(handle, pathname, response, postDate);
-        });
+        route(handle, pathname, response, request);
     }
 
     http.createServer(onRequest).listen(8888);
